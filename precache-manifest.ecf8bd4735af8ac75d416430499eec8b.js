c15ed002f416e1120b28668ab1a4f258db9756e4
mhrjnsa1 <mhrjnsa1@gmail.com>
2019-02-27 14:37:49 +0530

    first commit

---
+++ precache-manifest.ecf8bd4735af8ac75d416430499eec8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "6d369643ec0332e2c69c",
+    "url": "/static/js/main.6d369643.chunk.js"
+  },
+  {
+    "revision": "a2aeee8c9b0a3346cc2e",
+    "url": "/static/js/2.a2aeee8c.chunk.js"
+  },
+  {
+    "revision": "6d369643ec0332e2c69c",
+    "url": "/static/css/main.280b76e0.chunk.css"
+  },
+  {
+    "revision": "a2aeee8c9b0a3346cc2e",
+    "url": "/static/css/2.d6e2d380.chunk.css"
+  },
+  {
+    "revision": "1e1b6163e98822f3b2f7ab67cf2207a8",
+    "url": "/index.html"
+  }
+];
